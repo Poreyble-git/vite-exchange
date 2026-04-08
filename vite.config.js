4095ce034f1e5bccceeb314799e62dfd5e22d27a
Poreyble-git <trix49218@gmail.com>
2026-04-08 16:40:17 +0300

    vite.config bug fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import { fileURLToPath } from 'url'
+
+const root = fileURLToPath(new URL('.', import.meta.url))
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        admin: resolve(__dirname, 'admin.html'),
+        main: resolve(root, 'index.html'),
+        admin: resolve(root, 'admin.html'),
       },
     },
   },
